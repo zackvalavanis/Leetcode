ab589873f049f5546dcd1a94ebf43cbd62c3c615
Zack <168221270+Zval321@users.noreply.github.com>
2025-10-24 15:27:30 -0500

    leetcode practice

--- October-24-2025/maxOnes.js
+++ October-24-2025/maxOnes.js
@@ -1,11 +1,23 @@
 var nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
 
 const maxOnes = (nums, k) => { 
-  let left = 0
-  
+  let left = 0;
+  let zeroesCount = 0; 
+  let maxLength = 0; 
   for(let right = 0; right < nums.length; right++){ 
-    
+    if(nums[right] === 0){ 
+      zeroesCount++
+    }
+
+    while(zeroesCount > k){ 
+      if(nums[left] === 0){ 
+        zeroesCount--
+      }
+      left++
+    }
+    maxLength = Math.max(maxLength, right - left + 1)
   }
+  return maxLength; 
 }
 
 console.log(maxOnes(nums, k))
