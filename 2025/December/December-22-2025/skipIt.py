c5065bf56b036927e302275d6923c84c31750697
Zack <168221270+Zval321@users.noreply.github.com>
2025-12-22 10:29:48 -0600

    leetcode practice

---
+++ 2025/December/December-22-2025/skipIt.py
@@ -0,0 +1,33 @@
+# Description
+# Given an array of numbers, write a function that returns a new array in which only select numbers from the original array are included, based on the following details:
+
+# The new array should always start with the first number from the original array. 
+# The next number that should be included depends on what the first number is. The first number dictates how many spaces to the right the computer should move to pick the next number. For example, if the first number is 2, then the next number that the computer should select would be two spaces to the right. This number gets added to the new array. If this next number happens to be a 4, then the next number after that is the one four spaces to the right. And so on and so forth until the end of the array is reached.
+
+# Input:
+# [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
+
+# Output:
+# [2, 3, 1, 2, 2, 1, 5, 2, 2]
+
+from typing import List
+
+nums = [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
+
+def skipIt(nums: List[int]) -> List[int]: 
+  k = nums[0]
+  new_array = []
+  left = 0
+
+  while left <= len(nums): 
+    new_array.append(nums[left])
+    if left + k > len(nums): 
+      break
+    else: 
+      left += k
+      k = nums[left]
+  return new_array
+
+print(skipIt(nums))
+
+
